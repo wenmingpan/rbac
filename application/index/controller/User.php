9a94d45bd303a5afb4e0ea36663d347625603478
pwm5712 <pwm5712@163.com>
2017-02-10 13:39:53 +0800

    add 角色

--- application/index/controller/User.php
+++ application/index/controller/User.php
@@ -31,9 +31,11 @@ class User extends Controller
         // 添加入库
         if (Request::instance()->isPost()) {
             $params = Request::instance()->param();
+
             $name = $params['name'];
             $email = $params['email'];
             $status = $params['status'];
+            $role_ids = empty($params['role_id']) ? array(): $params['role_id']; // 角色id
             $data = ['name' => $name, 
                     'email' => $email, 
                     'status' => $status,
@@ -41,13 +43,18 @@ class User extends Controller
                     'updated_time' => time(),
                 ];
             $res = Db::table('user')->insert($data);
-            if($res) {
+            $userid = Db::table('user')->getLastInsID();
+            if($res) {     
+                $this->_setUserRole($userid, $role_ids);
                 $this->success('新增成功', 'User/index');
             } else{
                 $this->error('新增失败');
             }
         }
         
+        // 角色
+        $role = Db::table('role')->select();
+        $this->assign('role',$role);
         return view('add');
     }
     
@@ -82,41 +89,51 @@ class User extends Controller
             $email = $params['email'];
             $status = $params['status'];
             $role_ids = empty($params['role_id']) ? array(): $params['role_id']; // 角色id
-            
-            // 通过userid 查询role_id
-            $res = Db::table('user_role')->where('uid', $userid)->select();
-            $user_roleid = array_column($res, 'role_id');
-            $result = array_diff($user_roleid, $role_ids); // 求差集,
-            
+             
             // 修改用户信息
             $data = ['name' => $name, 
                     'email' => $email, 
                     'status' => $status,
                     'updated_time' => time(),
                 ];
-            
-            // 修改用户权限信息
-            if ($result) {  // 有值说明删除
-                foreach ($result as $key => $value) {
-                    Db::table('user_role')->where(['uid' => $userid, 'role_id' => $value])->delete();
-                }
-            } else { // 无值进行添加
-                foreach ($role_ids as $key => $value) {
-                    if (!in_array($value, $result)) {
-                        $data1 = ['uid' => $userid, 'role_id' => $value, 'created_time'=> time()];
-                        Db::table('user_role')->insert($data1);
-                    }
-                }
-            }
-            
+            // 更新用户
             $updat_user = Db::table('user')
                     ->where('id', $userid)
                     ->update($data);
-            
+            // 设置用户角色关系
+            $this->_setUserRole($userid, $role_ids);
             if($updat_user) {
                 $this->success('修改成功', 'User/index');
             } else{
                 $this->error('修改失败');
+            }
+        }
+    }
+    
+    /**
+     * 设置用户与角色之间的关联关系
+     * 
+     * @param type $userid
+     * @param type $role_ids
+     */
+    private function _setUserRole($userid, $role_ids=array())
+    {
+        // 通过userid 查询role_id
+        $res = Db::table('user_role')->where('uid', $userid)->select();
+        $user_roleid = array_column($res, 'role_id');
+        $result = array_diff($user_roleid, $role_ids); // 求差集,
+
+        // 修改用户权限信息
+        if ($result) {  // 有值说明删除
+            foreach ($result as $key => $value) {
+                Db::table('user_role')->where(['uid' => $userid, 'role_id' => $value])->delete();
+            }
+        } else { // 无值进行添加
+            foreach ($role_ids as $key => $value) {
+                if (!in_array($value, $result)) {
+                    $data1 = ['uid' => $userid, 'role_id' => $value, 'created_time'=> time()];
+                    Db::table('user_role')->insert($data1);
+                }
             }
         }
     }
